8ec6416856d76fa672d544a6962e665cc2a17dd5
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-11 17:58:49 -0700

    final commit

--- our_markov_code.py
+++ our_markov_code.py
@@ -135,61 +135,9 @@ def execute_functions(file_name):
 
     tweet(random_text)
 
-execute_functions('gettysburg.txt')
+execute_functions('lyrics.txt')
 
 
 
 
 
-#if want to add n grams, the start is here.
-# def make_chains(text_string, n):
-#     """Take input text as string; return dictionary of Markov chains.
-
-#     A chain will be a key that consists of a tuple of (word1, word2)
-#     and the value would be a list of the word(s) that follow those two
-#     words in the input text.
-
-#     For example:
-
-#         >>> chains = make_chains("hi there mary hi there juanita")
-
-#     Each bigram (except the last) will be a key in chains:
-
-#         >>> sorted(chains.keys())
-#         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-#     Each item in chains is a list of all possible following words:
-
-#         >>> chains[('hi', 'there')]
-#         ['mary', 'juanita']
-        
-#         >>> chains[('there','juanita')]
-#         [None]
-#     """
-
-#     chains = {}
-
-#     text_string = text_string.split()
-#     text_string.append(None)
-
-#     # for loop to go through text and group into tuples
-#     for i in range(len(text_string)-n):
-
-#         following_word = text_string[i + n]
-#         chain_keys_list = []
-        
-#         while len(chain_keys_list) < n:
-#             chain_keys_list.append(text_string[i])
-#             i += 1
-#         chain_tuple = tuple(chain_keys_list)
-#         # chain_tuple = (text_string[i], text_string[i + 1])
-
-#     # conditional statement to check for chain_tuple in keys of dictionary
-#         # if new entry, it'll add a list containing a following word
-#         if chain_tuple not in chains.keys():
-#             chains[chain_tuple] = [following_word]
-#         # if not new entry, append the following word to the value
-#         else:
-#             chains[chain_tuple].append(following_word)
-
-#     return chains
